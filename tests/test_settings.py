a4491539d219837257786b3b1d0cae9379d238a2
Thomas Vavrys <vavrys@cleancode.cz>
2015-08-13 15:52:36 +0200

    Fix runtests.py for django < 1.7

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,9 +1,3 @@
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-    }
-}
-DATABASES = {}
 SECRET_KEY = "123456789"
 INSTALLED_APPS = (
     'tests',
